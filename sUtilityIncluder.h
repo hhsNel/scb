e420e44e77415c2a2dd954c9b8a433ccd2994b5d
hhsNel <hihijsonel@gmail.com>
2025-01-19 17:43:27 +0100

    integers in includer

--- sUtilityIncluder.h
+++ sUtilityIncluder.h
@@ -8,10 +8,14 @@ Including this file, you need to specify the following:)
  $h (Defined name)		$h (Purpose)
  $t (SUI_INCLUDE_TARGET)	$t (Name $(In "" quotes$) of the file to be included. Needs to be relative path from this file)
  $t (SUI_BASE_NAME)		$t (The base name, to be tweaked by different SUI_ settings)
-	 $t (SUI_VARIATE_FRACTIONAL_0)	$t (If SUI_YES, the target will be included thrice with SUI_TARGET_FRACTIONAL_0 set to sfloat, sdouble and sldouble, and SUI_TARGET_NAME appended with -f, -d and -ld. If SUI_NO, SUI_TARGET_FRACTIONAL_0 will be DUI_DEFAULT_FRACTIONAL_0)
-	 $t (SUI_VARIATE_FRACTIONAL_1)	$t (If SUI_YES, the target will be included thrice with SUI_TARGET_FRACTIONAL_1 set to sfloat, sdouble and sldouble, and SUI_TARGET_NAME appended with -f, -d and -ld. If SUI_NO, SUI_TARGET_FRACTIONAL_1 will be DUI_DEFAULT_FRACTIONAL_1)
-	 $t (SUI_VARIATE_FRACTIONAL_2)	$t (If SUI_YES, the target will be included thrice with SUI_TARGET_FRACTIONAL_2 set to sfloat, sdouble and sldouble, and SUI_TARGET_NAME appended with -f, -d and -ld. If SUI_NO, SUI_TARGET_FRACTIONAL_2 will be DUI_DEFAULT_FRACTIONAL_2)
-	 $t (SUI_VARIATE_FRACTIONAL_3)	$t (If SUI_YES, the target will be included thrice with SUI_TARGET_FRACTIONAL_3 set to sfloat, sdouble and sldouble, and SUI_TARGET_NAME appended with -f, -d and -ld. If SUI_NO, SUI_TARGET_FRACTIONAL_3 will be DUI_DEFAULT_FRACTIONAL_3)
+	 $t (SUI_VARIATE_FRACTIONAL_0)	$t (If SUI_YES, the target will be included thrice with SUI_TARGET_FRACTIONAL_0 set to sfloat, sdouble and sldouble, and SUI_TARGET_NAME appended with -f, -d and -ld. If SUI_NO, SUI_TARGET_FRACTIONAL_0 will be SUI_DEFAULT_FRACTIONAL_0)
+	 $t (SUI_VARIATE_FRACTIONAL_1)	$t (If SUI_YES, the target will be included thrice with SUI_TARGET_FRACTIONAL_1 set to sfloat, sdouble and sldouble, and SUI_TARGET_NAME appended with -f, -d and -ld. If SUI_NO, SUI_TARGET_FRACTIONAL_1 will be SUI_DEFAULT_FRACTIONAL_1)
+	 $t (SUI_VARIATE_FRACTIONAL_2)	$t (If SUI_YES, the target will be included thrice with SUI_TARGET_FRACTIONAL_2 set to sfloat, sdouble and sldouble, and SUI_TARGET_NAME appended with -f, -d and -ld. If SUI_NO, SUI_TARGET_FRACTIONAL_2 will be SUI_DEFAULT_FRACTIONAL_2)
+	 $t (SUI_VARIATE_FRACTIONAL_3)	$t (If SUI_YES, the target will be included thrice with SUI_TARGET_FRACTIONAL_3 set to sfloat, sdouble and sldouble, and SUI_TARGET_NAME appended with -f, -d and -ld. If SUI_NO, SUI_TARGET_FRACTIONAL_3 will be SUI_DEFAULT_FRACTIONAL_3)
+	 $t (SUI_VARIATE_INTEGER_0)	$t (If SUI_YES, the target will be included four times with SUI_TARGET_INTEGER_0 set to sshortint, sint, slongint, sllint and SUI_TARGET_NAME appended with -s, -l and -ll. If SUI_NO, SUI_TARGET_INTEGER_0 will be SUI_DEFAULT_INTEGER_0)
+	 $t (SUI_VARIATE_INTEGER_1)	$t (If SUI_YES, the target will be included four times with SUI_TARGET_INTEGER_1 set to sshortint, sint, slongint, sllint and SUI_TARGET_NAME appended with -s, -l and -ll. If SUI_NO, SUI_TARGET_INTEGER_1 will be SUI_DEFAULT_INTEGER_1)
+	 $t (SUI_VARIATE_INTEGER_2)	$t (If SUI_YES, the target will be included four times with SUI_TARGET_INTEGER_2 set to sshortint, sint, slongint, sllint and SUI_TARGET_NAME appended with -s, -l and -ll. If SUI_NO, SUI_TARGET_INTEGER_2 will be SUI_DEFAULT_INTEGER_2)
+	 $t (SUI_VARIATE_INTEGER_3)	$t (If SUI_YES, the target will be included four times with SUI_TARGET_INTEGER_3 set to sshortint, sint, slongint, sllint and SUI_TARGET_NAME appended with -s, -l and -ll. If SUI_NO, SUI_TARGET_INTEGER_3 will be SUI_DEFAULT_INTEGER_3)
 */
 
 #ifndef SUI_YES
@@ -64,6 +68,62 @@ Including this file, you need to specify the following:)
 	#include __FILE__
 	#define SUI_FRACTIONAL_3_SUFFIX ld
 	#define SUI_TARGET_FRACTIONAL sldouble
+	#include __FILE__
+#elif SUI_VARIATE_INTEGER_0
+	#define SUI_VARIATE_INTEGER_0
+	#define SUI_INTEGER_0_SUFFIX s
+	#define SUI_TARGET_INTEGER_0 sshortint
+	#include __FILE__
+	#define SUI_INTEGER_0_SUFFIX 
+	#define SUI_TARGET_INTEGER_0 sint
+	#include __FILE__
+	#define SUI_INTEGER_0_SUFFIX l
+	#define SUI_TARGET_INTEGER_0 slongint
+	#include __FILE__
+	#define SUI_INTEGER_0_SUFFIX ll
+	#define SUI_TARGET_INTEGER_0 sllint
+	#include __FILE__
+#elif SUI_VARIATE_INTEGER_1
+	#define SUI_VARIATE_INTEGER_1
+	#define SUI_INTEGER_1_SUFFIX s
+	#define SUI_TARGET_INTEGER_1 sshortint
+	#include __FILE__
+	#define SUI_INTEGER_1_SUFFIX 
+	#define SUI_TARGET_INTEGER_1 sint
+	#include __FILE__
+	#define SUI_INTEGER_1_SUFFIX l
+	#define SUI_TARGET_INTEGER_1 slongint
+	#include __FILE__
+	#define SUI_INTEGER_1_SUFFIX ll
+	#define SUI_TARGET_INTEGER_1 sllint
+	#include __FILE__
+#elif SUI_VARIATE_INTEGER_2
+	#define SUI_VARIATE_INTEGER_2
+	#define SUI_INTEGER_2_SUFFIX s
+	#define SUI_TARGET_INTEGER_2 sshortint
+	#include __FILE__
+	#define SUI_INTEGER_2_SUFFIX 
+	#define SUI_TARGET_INTEGER_2 sint
+	#include __FILE__
+	#define SUI_INTEGER_2_SUFFIX l
+	#define SUI_TARGET_INTEGER_2 slongint
+	#include __FILE__
+	#define SUI_INTEGER_2_SUFFIX ll
+	#define SUI_TARGET_INTEGER_2 sllint
+	#include __FILE__
+#elif SUI_VARIATE_INTEGER_3
+	#define SUI_VARIATE_INTEGER_3
+	#define SUI_INTEGER_3_SUFFIX s
+	#define SUI_TARGET_INTEGER_3 sshortint
+	#include __FILE__
+	#define SUI_INTEGER_3_SUFFIX 
+	#define SUI_TARGET_INTEGER_3 sint
+	#include __FILE__
+	#define SUI_INTEGER_3_SUFFIX l
+	#define SUI_TARGET_INTEGER_3 slongint
+	#include __FILE__
+	#define SUI_INTEGER_3_SUFFIX ll
+	#define SUI_TARGET_INTEGER_3 sllint
 	#include __FILE__
 #else
 
