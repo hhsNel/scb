5f88cd67f656907ed411e9d2bdaf1612d087c4c9
hhsNel <hihijsonel@gmail.com>
2025-01-20 23:22:33 +0100

    howto includer

--- exheader.h
+++ exheader.h
@@ -4,7 +4,13 @@
 #define _prefix 
 #define _PREFIX SCONCAT3X(_PREFIX,_,_prefix)
 
-// SMAKE_NAME(_PREFIX,)
+//SMAKE_NAME(_PREFIX,)
+
+//#define SUI_TARGET_NAME "someHeader.h"
+//#define SUI_VARIATE_FRACTIONAL_0 SUI_YES
+//#include "sUtilityIncluder.h"
+// ->
+//someFunctionf ; someFunctiond ; someFunctionld
 
 #endif
 
