5470f4b2b946b7e1e9675f2f5f52cecf0dd38a87
hhsNel <hihijsonel@gmail.com>
2025-02-22 23:08:56 +0100

    malloc info

--- core/malloc/malloc.c
+++ core/malloc/malloc.c
@@ -1,5 +1,13 @@
 struct SMAKE_NAME(S_CORE_PREFIX, malloc_data_chunk) {
-	SMAKE_NAME(S_CORE_PREFIX, malloc_data_chunk) *next;
+	struct SMAKE_NAME(S_CORE_PREFIX, malloc_data_chunk) *next;
 	ssize chunk_size;
 	sbool is_free : 1;
 }
+
+struct SMAKE_NAME(S_CORE_PREFIX, malloc_info) {
+	SMAKE_NAME(S_CORE_PREFIX, malloc_data_chunk) *first;
+	ssize total_size;
+}
+
+
+
