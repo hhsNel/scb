c974723933df7d45e23c79ec94ee8773db3a46b5
hhsNel <hihijsonel@gmail.com>
2025-02-22 23:12:31 +0100

    malloc memory region allocation beginning

--- core/malloc/malloc.c
+++ core/malloc/malloc.c
@@ -5,9 +5,14 @@ struct SMAKE_NAME(S_CORE_PREFIX, malloc_data_chunk) {
 }
 
 struct SMAKE_NAME(S_CORE_PREFIX, malloc_info) {
-	SMAKE_NAME(S_CORE_PREFIX, malloc_data_chunk) *first;
+	struct SMAKE_NAME(S_CORE_PREFIX, malloc_data_chunk) *first;
 	ssize total_size;
+	ssize alloc_size;
 }
 
+struct SMAKE_NAME(S_CORE_PREFIX, malloc_info) SMAKE_NAME(S_CORE_PREFIX, global_malloc);
 
+void SMAKE_NAME(S_CORE_PREFIX, malloc_map_next) (svoidptr next_region) {
+	ssize size = SMAKE_NAME(S_CORE_PREFIX, global_malloc).alloc_size;
+}
 
